f9d6daeea33872ab4a262cedc2eb1ddd7a28cf9b
Denis <marincasdenis@yahoo.com>
2023-12-19 15:21:49 +0200

    Modified authContext

--- movieApp/src/contexts/authContext.js
+++ movieApp/src/contexts/authContext.js
@@ -1,62 +1,52 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { auth } from '../firebase/firebase';
+import React, { useState, createContext } from "react";
+import { login, signup } from "../api/movies-api";
 
-const authContext = React.createContext();
+export const AuthContext = createContext(null);
 
+const AuthContextProvider = (props) => {
+  const existingToken = localStorage.getItem("token");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authToken, setAuthToken] = useState(existingToken);
+  const [userName, setUserName] = useState("");
 
-export function useAuth() {
-  return useContext(authContext);
-}
-
-export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState({});
-
-  function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password);
+  //Function to put JWT token in local storage.
+  const setToken = (data) => {
+    localStorage.setItem("token", data);
+    setAuthToken(data);
   }
 
-  function login(email, password) {
-    return auth.signInWithEmailAndPassword(email, password);
-  }
-
-  function logout() {
-    return auth.signOut();
-  }
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      setCurrentUser(user);
-    });
-
-    return unsubscribe;
-  }, []);
-
-  const value = {
-    currentUser,
-    signup,
-    login,
-    logout
+  const authenticate = async (username, password) => {
+    const result = await login(username, password);
+    if (result.token) {
+      setToken(result.token)
+      setIsAuthenticated(true);
+      setUserName(username);
+    }
   };
 
+  const register = async (username, password) => {
+    const result = await signup(username, password);
+    console.log(result.code);
+    return (result.code == 201) ? true : false;
+  };
+
+  const signout = () => {
+    setTimeout(() => setIsAuthenticated(false), 100);
+  }
+
   return (
-    <authContext.Provider value={value}>
-      {children}
-    </authContext.Provider>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        authenticate,
+        register,
+        signout,
+        userName
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
   );
-}
-
-// Export individual functions for direct use in components
-export const useSignup = () => {
-  const { signup } = useAuth();
-  return signup;
 };
 
-export const useLogin = () => {
-  const { login } = useAuth();
-  return login;
-};
-
-export const useLogout = () => {
-  const { logout } = useAuth();
-  return logout;
-};
+export default AuthContextProvider;
