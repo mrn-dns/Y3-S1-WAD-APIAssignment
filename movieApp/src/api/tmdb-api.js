29b292a270c2273b23c1cd37020c94ab4c136b68
Denis <marincasdenis@yahoo.com>
2023-12-19 15:21:04 +0200

    Added login and signup function

--- movieApp/src/api/tmdb-api.js
+++ movieApp/src/api/tmdb-api.js
@@ -1,3 +1,25 @@
+export const login = async (username, password) => {
+  const response = await fetch('http://localhost:8080/api/users', {
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      method: 'post',
+      body: JSON.stringify({ username: username, password: password })
+  });
+  return response.json();
+};
+
+export const signup = async (username, password) => {
+  const response = await fetch('http://localhost:8080/api/users?action=register', {
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      method: 'post',
+      body: JSON.stringify({ username: username, password: password })
+  });
+  return response.json();
+};
+
 export const getMovieActors = () => {
   return fetch(
     `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=2`
