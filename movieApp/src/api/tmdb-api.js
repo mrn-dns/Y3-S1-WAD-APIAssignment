8e7f6ccaaaf46e9a239ad34b350906cd0e84c7ac
Denis <marincasdenis@yahoo.com>
2023-12-20 21:07:03 +0200

    ReactApp uses API for upcoming movies

--- movieApp/src/api/tmdb-api.js
+++ movieApp/src/api/tmdb-api.js
@@ -186,19 +186,31 @@ export const getMovieImages = ({ queryKey }) => {
       });
   };
 
-  export const getUpcomingMovies = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-    ).then((response) => {
-      if (!response.ok) {
-        throw new Error(response.json().message);
+  export const getUpcomingMovies = async () => {
+    const response = await fetch(
+      'http://localhost:8080/api/movies/tmdb/upcoming', {
+      headers: {
+        'Authorization': window.localStorage.getItem('token'),
       }
-      return response.json();
-    })
-    .catch((error) => {
-       throw error
-    });
+    },
+    console.log(window.localStorage.getItem('token'))
+    )
+    return response.json();
   };
+  
+  // export const getUpcomingMovies = () => {
+  //   return fetch(
+  //     `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  //   ).then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error(response.json().message);
+  //     }
+  //     return response.json();
+  //   })
+  //   .catch((error) => {
+  //      throw error
+  //   });
+  // };
 
   export const getTopRatedMovies = () => {
     return fetch(
